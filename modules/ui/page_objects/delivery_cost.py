5ac88a7f55872636570c750fcced183123bc6368
Sveta <sa@Svetas-MacBook-Pro.local>
2023-11-05 19:58:37 +0200

    Rewrite test for delivery cost

--- modules/ui/page_objects/delivery_cost.py
+++ modules/ui/page_objects/delivery_cost.py
@@ -1,5 +1,6 @@
 from modules.ui.page_objects.base_page import BasePage
 from selenium.webdriver.common.by import By
+import time
 
 class DeliveryCost(BasePage):
     URL = "https://novaposhta.ua/delivery"
@@ -15,10 +16,11 @@ class DeliveryCost(BasePage):
     
     def route_sender(self, senderCity):
         #Find sender city element
-        sender_city = self.driver.find_element(By.ID, "DeliveryForm_senderCity")
+        sender_city = self.driver.find_element(By.NAME, "DeliveryForm[senderCity]")
         
         #Enter the city
         sender_city.send_keys(senderCity)
+        time.sleep(1)
 
         #Expected result after entered name "Вінниця" and click
         expected_res_sender = self.driver.find_element(By.ID, "lie71629ab-4b33-11e4-ab6d-005056801329")
@@ -26,46 +28,45 @@ class DeliveryCost(BasePage):
 
     def route_recipient(self):
         #Find recipient city element
-        recipient_city = self.driver.find_element(By.ID, "DeliveryForm_recipientCity")
+        recipient_city = self.driver.find_element(By.NAME, "DeliveryForm[recipientCity]")
         recipient_city.click()
-        
+    
         #Find element on dropdown menu
-        dropdown_recipient = self.driver.find_element(By.CLASS_NAME, "cdb5c88f5-391c-11dd-90d9-001a92567626")
+        dropdown_recipient = self.driver.find_element(By.XPATH, '//li[@data-value="db5c88f5-391c-11dd-90d9-001a92567626"]')
+        time.sleep(1)
         recipient_text = dropdown_recipient.text
         assert recipient_text == "Львів"
         dropdown_recipient.click()
     
-    def place_character(self, qnt, value, weight, length, width, height):
-        #Find quantity element
-        quantity_elem = self.driver.find_element(By.CLASS_NAME, "short allowed-char count")
-        quantity_elem.send_keys(qnt)
-        if int(qnt) > 999:
-            quantity_elem.send_keys("999")
-
-        #Find declared value element and send keys
-        value_elem = self.driver.find_element(By.CLASS_NAME, "short allowed-char cost")
+    def place_character(self, value, weight, length, width, height):
+       #Find declared value element and send keys
+        value_elem = self.driver.find_element(By.NAME, "DeliveryForm[optionsSeat][1][cost]")
         value_elem.send_keys(value)
 
         #Find weight element and send keys
-        weight_elem = self.driver.find_element(By.CLASS_NAME, "short allowed-char weight")
+        weight_elem = self.driver.find_element(By.NAME, "DeliveryForm[optionsSeat][1][weight]")
         weight_elem.send_keys(weight)
 
         #Find length element and send keys
-        length_elem = self.driver.find_element(By.CLASS_NAME, "allowed-char volumetricLength")
+        length_elem = self.driver.find_element(By.NAME, "DeliveryForm[optionsSeat][1][volumetricLength]")
         length_elem.send_keys(length)
 
         #Find width element and send keys
-        width_elem = self.driver.find_element(By.CLASS_NAME, "allowed-char volumetricWidth")
+        width_elem = self.driver.find_element(By.NAME, "DeliveryForm[optionsSeat][1][volumetricWidth]")
         width_elem.send_keys(width)
         
         #Find height element and send keys
-        height_elem = self.driver.find_element(By.CLASS_NAME, "allowed-char volumetricHeight")
+        height_elem = self.driver.find_element(By.NAME, "DeliveryForm[optionsSeat][1][volumetricHeight]")
         height_elem.send_keys(height)
     
     def btn_submit(self):
-        btn_submit = self.driver.find_element(By.CLASS_NAME, "btn submit")
+        btn_submit = self.driver.find_element(By.NAME, "yt0")
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", btn_submit)
         btn_submit.click()
     
-    def check_results(self, expected_title):
-        return self.driver.title == expected_title
+    def check_results(self, expected_text):
+        h1_element = self.driver.find_element(By.XPATH, "//h1[@class='page_title']")
+        actual_text = h1_element.text
+        assert actual_text == expected_text
+
 
--- tests/ui/test_ui_np_delivery.py
+++ tests/ui/test_ui_np_delivery.py
@@ -1,5 +1,6 @@
 from modules.ui.page_objects.delivery_cost import DeliveryCost 
 import pytest
+import time
 
 @pytest.mark.mui
 def test_check_delivery_cost():
@@ -8,17 +9,22 @@ def test_check_delivery_cost():
     #Open page https://novaposhta.ua/delivery
     delivery_cost.go_to()
 
+    time.sleep(1)
+
     #Check page title
     delivery_cost.check_page_title("Вартість доставки")
 
     #Enter all inputs
     delivery_cost.route_sender("Вінниця")
-    recipient_text = delivery_cost.route_recipient()
-    assert recipient_text == "Львів"
-    delivery_cost.place_character(2, 14, 10, 50, 60, 70)
+    delivery_cost.route_recipient()
+    time.sleep(2)
+    
+    delivery_cost.place_character(14, 10, 50, 60, 70)
+    time.sleep(1)
 
     delivery_cost.btn_submit()
+    time.sleep(2)
 
-    assert delivery_cost.check_results("Розрахункова вартість доставки")
+    delivery_cost.check_results("Вартість доставки")
 
     delivery_cost.close()
